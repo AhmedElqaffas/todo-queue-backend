02f520497c4563fe8f66c6a8d9e5e5bdb9e7a5ab
Ahmed El-Qaffas <ahmed.elqaffas97@gmail.com>
2025-03-19 22:32:33 +0200

    use spring cloud function adapter for web - version > 4.1.0 has a bug so I had to use 4.1.0 and therefore had to downgrade spring boot to 3.3.0

--- src/main/java/demo/todo/group/TodoController.java
+++ src/main/java/demo/todo/group/TodoController.java
@@ -3,14 +3,14 @@ package demo.todo.group;
 import demo.todo.group.entities.TodoItem;
 import demo.todo.group.queue.TodoQueuePublisher;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MissingRequestHeaderException;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.WebRequest;
 
 import java.util.List;
 import java.util.UUID;
 
-/**
- * For local testing using postman
- */
 @RestController
 @RequestMapping("todo")
 public class TodoController {
@@ -22,19 +22,28 @@ public class TodoController {
     private TodoService todoService;
 
     @GetMapping("/all")
-    public List<TodoItem> getTodos(){
-        return todoService.getTodos();
+    public ResponseEntity<List<TodoItem>> getTodos(@RequestHeader("Todo-User-Email") String userEmail
+                                                    ){
+        return ResponseEntity.ok(todoService.getTodos());
     }
 
     @PostMapping("/new")
-    public TodoItem createTodos(@RequestBody TodoItem todo){
+    public TodoItem createTodos(@RequestBody TodoItem todo,
+                                @RequestHeader("Todo-User-Email") String userEmail){
         queuePublisher.sendCreateTodoEvent(todo);
         return todo;
     }
 
     @PostMapping("/remove")
-    public void removeTodo(@RequestBody List<UUID> todoIDs){
+    public void removeTodo(@RequestBody List<UUID> todoIDs,
+            @RequestHeader("Todo-User-Email") String userEmail){
         queuePublisher.sendRemoveTodosEvent(todoIDs);
     }
 
+    @ExceptionHandler(MissingRequestHeaderException.class)
+    public ResponseEntity<String> handleMissingRequestHeader(MissingRequestHeaderException ex, WebRequest request) {
+        String headerName = ex.getHeaderName();
+        String errorMessage = "Missing required header: " + headerName;
+        return ResponseEntity.badRequest().body(errorMessage);
+    }
 }
