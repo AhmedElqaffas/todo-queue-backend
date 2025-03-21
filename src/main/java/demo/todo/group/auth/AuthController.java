09169205707256f7a177d24c12f51758f102ec95
Ahmed El-Qaffas <ahmed.elqaffas97@gmail.com>
2025-03-21 15:45:08 +0200

    handle cognito bad request error

--- src/main/java/demo/todo/group/auth/AuthController.java
+++ src/main/java/demo/todo/group/auth/AuthController.java
@@ -2,10 +2,9 @@ package demo.todo.group.auth;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 
 @RestController
 @RequestMapping("auth")
@@ -17,5 +16,10 @@ public class AuthController {
     @GetMapping("/token")
     public ResponseEntity<Tokens> getJwtToken(@RequestParam String code){
         return ResponseEntity.ok(authService.getTokens(code));
+    }
+
+    @ExceptionHandler(WebClientResponseException.BadRequest.class)
+    public ResponseEntity<String> badCognitoRequest(WebClientResponseException.BadRequest ex, WebRequest request) {
+        return ResponseEntity.badRequest().body("Couldn't get tokens for the given code.");
     }
 }
